5a05705c0d5d2c17d118be830346b56510fe079b
intansavita <intansavita@gmail.com>
2023-04-13 11:34:05 +0700

    perubahan file index

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+  $nama_kamu ="Intan";
+  $ummur = 10;
+  // tanda petik dua
+  /*
+  echo "hello world, Welcome".$nama_kamu;
+  echo "Umur Kamu". $ummur;
+  echo "Tahun";
+  echo "<br/>";
+  */
+  echo "Hello World, Welcome". $nama_kamu "Umur Kamu". $ummur. "tahun"; 
+  echo "<br/>";
+  echo "Berarti $nama_kamu lahir di tahun";
+  echo 2023-$ummur;
+
+  //tanda petik satu
+  //echo 'hello world';
+
+?>
